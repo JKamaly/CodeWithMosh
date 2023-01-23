e1b2469995220f47171ee9ae80fcca5aa4bbc91d
JKamaly <107716788+JKamaly@users.noreply.github.com>
2023-01-23 18:44:08 -0500

    Update Practice1.java
    remote repo commit

--- src/homayoon/Practice1.java
+++ src/homayoon/Practice1.java
@@ -5,7 +5,12 @@ public class Practice1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Checking the affect of change in project to push project in remote repo");
+		
+		
 
+	}
+	public void remoteMethod(){
+	// this is from remote repo
 	}
 
 }

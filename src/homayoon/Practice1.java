211b8ebe67e01df562c1c091144ef068b2e540f2
Kamaly <kamali.saidjalal@gmail.com>
2023-01-23 15:36:43 -0500

    Second commit

---
+++ src/homayoon/Practice1.java
@@ -0,0 +1,11 @@
+package homayoon;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Checking the affect of change in project to push project in remote repo");
+
+	}
+
+}
